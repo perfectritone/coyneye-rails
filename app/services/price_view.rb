f32879010b3078e7667d9033a7f1092dbabb27db
Dave Frey <perfectritone@gmail.com>
2018-11-07 17:24:36 -0500

    Showing time last updated

--- app/services/price_view.rb
+++ app/services/price_view.rb
@@ -1,13 +1,23 @@
 class PriceView
+  cattr_reader :price
+
   def self.current
-    eth = Currency.find_by(symbol: "ETH")
-    usdt = Currency.find_by(symbol: "USDT")
-
-    price = Price.where(
-      from_currency: usdt,
-      to_currency: eth,
-    ).first
-
     price&.amount
+  end
+
+  def self.time_updated
+    price&.updated_at
+  end
+
+  def self.price
+    @@price ||= begin
+      eth = Currency.find_by(symbol: "ETH")
+      usdt = Currency.find_by(symbol: "USDT")
+
+      Price.where(
+        from_currency: usdt,
+        to_currency: eth,
+      ).first
+    end
   end
 end
