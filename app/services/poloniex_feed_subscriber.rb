5d5f1e9edc6d00de35426c8a283226314df9afc9
Dave Frey <perfectritone@gmail.com>
2019-05-14 00:40:14 -0400

    USDC_ETH

--- app/services/poloniex_feed_subscriber.rb
+++ app/services/poloniex_feed_subscriber.rb
@@ -2,20 +2,24 @@ require 'faye/websocket'
 require 'eventmachine'
 
 class PoloniexFeedSubscriber
+  CURRENCY_PAIR_IDS = {
+    usdt_eth: 149,
+    usdc_eth: 225,
+  }
+
   def perform
     Thread.new do
       EventMachine.run do
         ws = Faye::WebSocket::Client.new('wss://api2.poloniex.com')
 
         ws.on :open do |event|
-          ws.send({ command: "subscribe", channel: "1002"}.to_json)
-          p [:open_now]
+          ws.send { command: "subscribe", channel: "1002"}.to_json
         end
 
         ws.on :message do |event|
           data = JSON.parse(event.data)
 
-          if data[2] && data[2][0] == 149
+          if data.dig(2, 0) == CURRENCY_PAIR_IDS[:usdc_eth]
             current_value = data[2][1].to_f
 
             PriceUpdater.perform current_value
